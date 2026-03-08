4906dca5a30767c186e7d5aefd5dcaa8b34e5e38
toibaonguyen <toibaonguyen@gmail.com>
2026-03-08 11:28:05 +0700

    [refactor/order-status-schema] add new table and refactor for matching status

--- src/main/resources/db/migration/V1__init_tables.sql
+++ src/main/resources/db/migration/V1__init_tables.sql
@@ -201,10 +201,7 @@ CREATE TABLE
 CREATE TABLE 
     t_order_statuses (
         id          UUID PRIMARY KEY DEFAULT gen_random_uuid (),
-        code        VARCHAR(50) NOT NULL UNIQUE,
-        name        VARCHAR(100) NOT NULL,
-        description TEXT,
-        is_active   BOOLEAN DEFAULT TRUE,
+        m_status_id UUID NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         created_by UUID NOT NULL,
@@ -215,7 +212,6 @@ CREATE TABLE
     m_t_order_statuses (
         id          UUID PRIMARY KEY DEFAULT gen_random_uuid (),
         code        VARCHAR(50) NOT NULL UNIQUE,
-        name        VARCHAR(100) NOT NULL,
         description TEXT,
         is_active   BOOLEAN DEFAULT TRUE,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -248,10 +244,19 @@ CREATE TABLE
     );
 
 CREATE TABLE 
+    order_statuses (
+        id          UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+        m_status_id UUID NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        created_by UUID NOT NULL,
+        updated_by UUID NOT NULL
+    );
+
+CREATE TABLE 
     m_order_statuses (
         id          UUID PRIMARY KEY DEFAULT gen_random_uuid (),
         code        VARCHAR(50) NOT NULL UNIQUE,
-        name        VARCHAR(100) NOT NULL,
         description TEXT,
         is_active   BOOLEAN DEFAULT TRUE,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
